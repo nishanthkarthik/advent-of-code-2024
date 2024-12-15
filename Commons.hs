fd917e16f08fb22691966becbd3239185d97d948
Karthik Nishanth <nishanthkarthik@live.com>
2024-12-14 16:00:35 -0800

    day 1

--- Commons.hs
+++ Commons.hs
@@ -1,1 +1,14 @@
 module Commons where
+
+import qualified Data.Attoparsec.Text as At
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Environment (getArgs)
+
+inp :: At.Parser a -> IO a
+inp parser = do
+    input <- getArgs >>= TIO.readFile . head
+    let result = At.eitherResult $ At.feed (At.parse parser input) mempty
+    case result of
+        Left l -> error l
+        Right r -> return r
--- Day1.hs
+++ Day1.hs
@@ -1,5 +1,22 @@
-import Data.Attoparsec.Text.Lazy
+import qualified Data.Attoparsec.Text as At
+import qualified Data.Map.Strict as M
+import Data.List (sort, group)
+import Data.Maybe (fromMaybe)
 
-t = Data.Attoparsec.Text.Lazy.char 'C'
+import Commons
 
-main = putStrLn "Foobar"
+parser :: At.Parser [(Int,Int)]
+parser = At.sepBy1 ((,) <$> (At.decimal <* At.skipSpace) <*> At.decimal) At.endOfLine
+
+main :: IO ()
+main = do
+    i <- inp parser
+
+    let solve1 = sum $ map abs $ zipWith (-) (sort $ map fst i) (sort $ map snd i)
+    print solve1
+
+    let freq = M.fromList $ map (\i -> (head i, length i)) $ group $ sort $ map snd i
+    let solve2 = sum $ map ((\i -> i * fromMaybe 0 (M.lookup i freq)) . fst) i
+    print solve2
+
+    return ()
