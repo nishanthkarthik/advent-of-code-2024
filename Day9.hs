5538f2ef4bb4d1cd6e2da5922208e4ed70c59056
Karthik Nishanth <nishanthkarthik@live.com>
2024-12-17 22:05:40 -0800

    day 9 part 2

--- Day9.hs
+++ Day9.hs
@@ -6,12 +6,21 @@ import qualified Data.Set as S
 import qualified Data.Sequence as Q
 import Control.Monad
 import Control.Applicative
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 import Data.Maybe (isNothing, fromJust, maybe)
+import Data.Foldable (toList)
+import Debug.Trace
 
 import Commons
 
 data Block = Free Int | Used Int Int deriving (Eq, Show)
+
+prettyShow :: Block -> String
+prettyShow (Free n) = replicate n '.'
+prettyShow (Used i n) = replicate n (intToDigit i)
+
+prettyBlocks :: [Block] -> String
+prettyBlocks = concatMap prettyShow
 
 merge :: [a] -> [a] -> [a]
 merge [] ys = ys
@@ -36,12 +45,27 @@ repeatUntil :: (a -> Maybe a) -> a -> a
 repeatUntil f a = go a
     where go a = maybe a go (f a)
 
+fileCompact :: (Q.Seq Block, Int) -> Q.Seq Block
+fileCompact (q, i) = if isNothing freeBlockIdx || (freeBlockI > usedI) then q else compacted
+    where Just usedI = Q.findIndexR (\case Used i' _ | i' == i -> True; _ -> False) q
+          Used _ nu = Q.index q usedI
+          freeBlockIdx = Q.findIndexL (\case Free nf | nf >= nu -> True; _ -> False) q
+          freeBlockI = fromJust freeBlockIdx
+          Free nf = Q.index q freeBlockI
+          filled = Q.insertAt usedI (Free nu) (Q.deleteAt usedI q)
+          updated = Q.insertAt freeBlockI (Used i nu) (Q.deleteAt freeBlockI filled)
+          compacted = if nf == nu then updated else Q.insertAt (freeBlockI + 1) (Free (nf - nu)) updated
+
 main :: IO ()
 main = do
     let blockTypes = merge (map Used [0..]) (repeat Free)
     d <- Q.fromList . zipWith ($) blockTypes <$> inp (At.many1 (digitToInt <$> At.digit))
     let cycles = repeatUntil compact d
-        expandedBlocks = concatMap (\case Free _ -> []; Used i n -> replicate n i) cycles
-        s1 = sum $ zipWith (*) expandedBlocks [0..]
+        solve q = sum $ zipWith (*) (concatMap (\case Free n -> replicate n 0; Used i n -> replicate n i) q) [0..]
+        s1 = solve cycles
     print s1
-    return ()
+
+    let usedCount = (Q.length d + 1) `div` 2
+        q2 = foldl (curry fileCompact) d [usedCount-1,usedCount-2..0]
+        s2 = solve q2
+    print s2
