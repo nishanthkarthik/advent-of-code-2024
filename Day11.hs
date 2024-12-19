ffc155310469513d36d6eec6b864cfe15b199468
Karthik Nishanth <nishanthkarthik@live.com>
2024-12-18 18:10:35 -0800

    day 11

--- Day11.hs
+++ Day11.hs
@@ -3,26 +3,31 @@
 import qualified Data.Attoparsec.Text as At
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
+import GHC.Num (integerSizeInBase#)
+import GHC.Word (Word(W#))
 import Control.Monad
 import Control.Applicative
-import GHC.Num (integerSizeInBase#)
-import GHC.Word
 
 import Commons
 
 digitCount :: Integer -> Word
 digitCount i = W# (integerSizeInBase# (let W# x = 10 in x) i)
 
-step :: Integer -> S.Set Integer
-step 0 = S.singleton 1
-step n
-    | even d = S.fromList [n `mod` (10 ^ div d 2), n `div` (10 ^ div d 2)]
-    | otherwise = S.singleton (n * 2024)
+step :: Integer -> Integer -> M.Map Integer Integer
+step 0 c = M.singleton 1 c
+step n c
+    | even d = M.fromListWith (+) [(n `mod` (10 ^ div d 2), c), (n `div` (10 ^ div d 2), c)]
+    | otherwise = M.singleton (n * 2024) c
     where d = digitCount n
+
+turn :: M.Map Integer Integer -> M.Map Integer Integer
+turn m = M.unionsWith (+) nextStep
+    where nextStep = map (uncurry step) $ M.assocs m
 
 main :: IO ()
 main = do
     i <- inp (At.sepBy1 At.decimal At.space)
-    let stepG = foldr (\cu ac -> step cu `S.union` ac) S.empty
-    -- TODO cool! this reaches steady state at 3811 elements
-    mapM_ (print . S.size) $ take 150 $ iterate stepG $ S.fromList i
+    let start = M.fromList $ map (,1) i
+        score = sum . M.elems
+    print $ score (iterate turn start !! 25)
+    print $ score (iterate turn start !! 75)
